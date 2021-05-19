b51e7b4d723c1dbe6d41613770cc06b03d548526
Viet-Tien Ngoc <tiena2cva@gmail.com>
2021-05-19 22:58:45 +0700

    change homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,31 +6,35 @@ const FeatureList = [
   {
     title: "Tutorials",
     // Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: <>Hands-on assignments in all levels.</>,
+    description: "Hands-on assignments in all levels.",
+    url: "docs/intro",
   },
   {
     title: "Projects",
     // Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: <>Real-world fantastic projects by us.</>,
+    description: "Real-world fantastic projects by us",
+    url: "docs/intro",
   },
   {
     title: "Certificates",
     // Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: <>Reviews about courses and certificates.</>,
+    description: "Reviews about courses and certificates.",
+    url: "docs/intro",
   },
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ Svg, title, description, url }) {
   return (
-    <div className={clsx(styles.featureElem, "col col--4")}>
+    <div className={clsx(styles.featureElem, "col")}>
       {/* <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div> */}
 
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+        <h3 className={styles.featureTitle}>{title}</h3>
         <p>{description}</p>
       </div>
+      <a href={url}></a>
     </div>
   );
 }

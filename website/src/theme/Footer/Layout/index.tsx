f3bf893cc950174703f37ae3ca66781e72ddbfe7
Viet-Tien Ngoc <tiena2cva@gmail.com>
2022-10-25 23:58:46 +0700

    darft

--- website/src/theme/Footer/Layout/index.tsx
+++ website/src/theme/Footer/Layout/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import clsx from "clsx";
 import type { Props } from "@theme/Footer/Layout";
+import { useColorMode } from "@docusaurus/theme-common";
+import Logo from "@site/src/components/Logo";
+import styles from "./index.module.css";
 
 export default function FooterLayout({
   style,
@@ -8,20 +11,32 @@ export default function FooterLayout({
   logo,
   copyright,
 }: Props): JSX.Element {
+  const { colorMode } = useColorMode();
   return (
     <footer
-      className={clsx("footer", {
-        "footer--dark": style === "dark",
+      className={clsx(styles.footerHomepage, "footer", {
+        "footer--dark": colorMode === "dark",
       })}
     >
-      <div className="container container-fluid">
-        {/* {links} */}
-        {(logo || copyright) && (
-          <div className="footer__bottom text--center">
-            {logo && <div className="margin-bottom--sm">{logo}</div>}
-            {copyright}
+      <div className="row">
+        <div className="col col--4">
+          <div className={clsx(styles.footerLogo, "col")}>
+            <Logo width="50px" height="50px" style={colorMode} />
+            {copyright && <span>{copyright}</span>}
           </div>
-        )}
+        </div>
+        <div className="col col--4">
+          <div className={styles.footerLinks}></div>
+        </div>
+        <div className="col col--4">
+          <div className={styles.footerTagline}>
+            <div>
+              <span>Made with </span>
+              <span>❤️</span>
+              <span> by ZootoPi team</span>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );

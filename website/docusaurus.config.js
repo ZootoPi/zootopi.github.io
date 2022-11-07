bd6fed6d71cf7972fcbb54a5411cd3d6f0455920
Viet-Tien Ngoc <tiena2cva@gmail.com>
2022-11-07 20:22:22 +0700

    update font

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -67,8 +67,9 @@ const config = {
           },
           {
             href: "https://github.com/zootopi",
-            label: "GitHub",
             position: "right",
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
           },
         ],
       },
